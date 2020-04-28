2ae4d73597b4236a479c276736604d77c8f04ba4
bcbabrich <39421140+bcbabrich@users.noreply.github.com>
2020-04-28 13:53:13 -0400

    comit

--- pipeline.h
+++ pipeline.h
@@ -143,6 +143,9 @@ public:
 
 	void ID() {
 		type = bitExtracted(mem.registers[63], 0, 3);
+		if (type == 7) {
+			cout << "gang";
+		}
 		cbit = bitExtracted(mem.registers[63], 3, 1);
 		opcode = bitExtracted(mem.registers[63], 4, 5);
 		for (int i = 0; i < 5; i++) {
